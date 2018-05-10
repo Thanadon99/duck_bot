491f0b78ac3d422548c1ce39bbecb16b7a63fb7e
Thanadon99 <thanadon.scp@gmail.com>
2018-05-11 00:46:11 +0700

    start6

--- index.php
+++ index.php
@@ -5,44 +5,9 @@
 
 require_once('./vendor/autoload.php');
 
-// Namespace
-//use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
-//use \LINE\LINEBot;
-//use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-
-use LINE\LINEBot;
-use LINE\LINEBot\HTTPClient;
-use LINE\LINEBot\HTTPClient\CurlHTTPClient;
-//use LINE\LINEBot\Event;
-//use LINE\LINEBot\Event\BaseEvent;
-//use LINE\LINEBot\Event\MessageEvent;
-use LINE\LINEBot\MessageBuilder;
-use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-use LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
-use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
-use LINE\LINEBot\MessageBuilder\LocationMessageBuilder;
-use LINE\LINEBot\MessageBuilder\AudioMessageBuilder;
-use LINE\LINEBot\MessageBuilder\VideoMessageBuilder;
-use LINE\LINEBot\ImagemapActionBuilder;
-use LINE\LINEBot\ImagemapActionBuilder\AreaBuilder;
-use LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder ;
-use LINE\LINEBot\ImagemapActionBuilder\ImagemapUriActionBuilder;
-use LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder;
-use LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder;
-use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
-use LINE\LINEBot\TemplateActionBuilder;
-use LINE\LINEBot\TemplateActionBuilder\DatetimePickerTemplateActionBuilder;
-use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
-use LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
-use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselTemplateBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder;
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 
 // Token
 $channel_token = 'lZ+WXE4At+V8NlwkInMHC5wJAvpeeKnCt197Y7l1CVfzSG6uhdee6tVMhG/Esk2GEmFAjl7gvElqWawH4o7AUJxGnKbhpogowCJqIA1cQ57oIF/4qF8CrOx7f0K4RCUjqUy3urKNf4xFaPhCl+faGAdB04t89/1O/w1cDnyilFU=';
@@ -77,47 +42,6 @@ if (!is_null($events['events'])) {
                 case 'idcard':
                     $respMessage = '5845122451245';
                     break;
-				case "t_b":
-					// กำหนด action 4 ปุ่ม 4 ประเภท
-					$actionBuilder = array(
-						new MessageTemplateActionBuilder(
-							'Message Template',// ข้อความแสดงในปุ่ม
-							'This is Text' // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
-						),
-						new UriTemplateActionBuilder(
-							'Uri Template', // ข้อความแสดงในปุ่ม
-							'https://www.ninenik.com'
-						),
-						new DatetimePickerTemplateActionBuilder(
-							'Datetime Picker', // ข้อความแสดงในปุ่ม
-							http_build_query(array(
-								'action'=>'reservation',
-								'person'=>5
-							)), // ข้อมูลที่จะส่งไปใน webhook ผ่าน postback event
-							'datetime', // date | time | datetime รูปแบบข้อมูลที่จะส่ง ในที่นี้ใช้ datatime
-							substr_replace(date("Y-m-d H:i"),'T',10,1), // วันที่ เวลา ค่าเริ่มต้นที่ถูกเลือก
-							substr_replace(date("Y-m-d H:i",strtotime("+5 day")),'T',10,1), //วันที่ เวลา มากสุดที่เลือกได้
-							substr_replace(date("Y-m-d H:i"),'T',10,1) //วันที่ เวลา น้อยสุดที่เลือกได้
-						),      
-						new PostbackTemplateActionBuilder(
-							'Postback', // ข้อความแสดงในปุ่ม
-							http_build_query(array(
-								'action'=>'buy',
-								'item'=>100
-							)), // ข้อมูลที่จะส่งไปใน webhook ผ่าน postback event
-							'Postback Text'  // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
-						),      
-					);
-					$imageUrl = 'https://www.mywebsite.com/imgsrc/photos/w/simpleflower';
-					$replyData = new TemplateMessageBuilder('Button Template',
-						new ButtonTemplateBuilder(
-								'button template builder', // กำหนดหัวเรื่อง
-								'Please select', // กำหนดรายละเอียด
-								$imageUrl, // กำหนด url รุปภาพ
-								$actionBuilder  // กำหนด action object
-						)
-					);              
-					break;  
                 default:
                     break;
             }
@@ -133,3 +57,4 @@ if (!is_null($events['events'])) {
 }
 
 echo "OK";
+
