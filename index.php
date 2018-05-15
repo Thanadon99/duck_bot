d93bf0358aa612b9a80ec55a0b2e3f9c6b34748b
Thanadon99 <thanadon.scp@gmail.com>
2018-05-15 16:00:09 +0700

    start...

--- index.php
+++ index.php
@@ -99,7 +99,7 @@ if(!is_null($events)){
                 $userMessage = strtolower($userMessage); // แปลงเป็นตัวเล็ก สำหรับทดสอบ
                 switch ($userMessage) {
                     case "t":
-                        $textReplyMessage = "Bot ตอบกลับคุณเป็นข้อความ";
+                        $textReplyMessage = "Bot ตอบกลับคุณเป็น<br/>ข้อความ";
                         $replyData = new TextMessageBuilder($textReplyMessage);
                         break;
                     case "i":
@@ -307,28 +307,46 @@ if(!is_null($events)){
                             )
                         );
                         break;
-					 case "p":
-                            if(!is_null($groupId) || !is_null($roomId)){
-                                if($eventObj->isGroupEvent()){
-                                    $response = $bot->getGroupMemberProfile($groupId, $userId);
-                                }
-                                if($eventObj->isRoomEvent()){
-                                    $response = $bot->getRoomMemberProfile($roomId, $userId);    
-                                }
-                            }else{
-                                $response = $bot->getProfile($userId);
-                            }
-                            if ($response->isSucceeded()) {
-                                $userData = $response->getJSONDecodedBody(); // return array     
-                                // $userData['userId']
-                                // $userData['displayName']
-                                // $userData['pictureUrl']
-                                // $userData['statusMessage']
-                                $textReplyMessage = 'สวัสดีครับ คุณ '.$userData['displayName'];     
-                            }else{
-                                $textReplyMessage = 'สวัสดีครับ คุณคือใคร';
-                            }
-                            $replyData = new TextMessageBuilder($textReplyMessage);                                                 
+					case "รายงานบิน":
+                        // กำหนด action 4 ปุ่ม 4 ประเภท
+                        $actionBuilder = array(
+                            new MessageTemplateActionBuilder(
+                                'Message Template',// ข้อความแสดงในปุ่ม
+                                'This is Text' // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
+                            ),
+                            new UriTemplateActionBuilder(
+                                'Uri Template', // ข้อความแสดงในปุ่ม
+                                'https://www.ninenik.com'
+                            ),
+                            new DatetimePickerTemplateActionBuilder(
+                                'Datetime Picker', // ข้อความแสดงในปุ่ม
+                                http_build_query(array(
+                                    'action'=>'reservation',
+                                    'person'=>5
+                                )), // ข้อมูลที่จะส่งไปใน webhook ผ่าน postback event
+                                'datetime', // date | time | datetime รูปแบบข้อมูลที่จะส่ง ในที่นี้ใช้ datatime
+                                substr_replace(date("Y-m-d H:i"),'T',10,1), // วันที่ เวลา ค่าเริ่มต้นที่ถูกเลือก
+                                substr_replace(date("Y-m-d H:i",strtotime("+5 day")),'T',10,1), //วันที่ เวลา มากสุดที่เลือกได้
+                                substr_replace(date("Y-m-d H:i"),'T',10,1) //วันที่ เวลา น้อยสุดที่เลือกได้
+                            ),      
+                            new PostbackTemplateActionBuilder(
+                                'Postback', // ข้อความแสดงในปุ่ม
+                                http_build_query(array(
+                                    'action'=>'buy',
+                                    'item'=>100
+                                )) // ข้อมูลที่จะส่งไปใน webhook ผ่าน postback event
+    //                          'Postback Text'  // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
+                            ),      
+                        );
+                        $imageUrl = 'https://www.mywebsite.com/imgsrc/photos/w/simpleflower';
+                        $replyData = new TemplateMessageBuilder('Button Template',
+                            new ButtonTemplateBuilder(
+                                    'button template builder', // กำหนดหัวเรื่อง
+                                    'Please select', // กำหนดรายละเอียด
+                                    $imageUrl, // กำหนด url รุปภาพ
+                                    $actionBuilder  // กำหนด action object
+                            )
+                        );              
                         break; 
                     default:
                         $textReplyMessage = " คุณไม่ได้พิมพ์ ค่า ตามที่กำหนด";
