e23d46d5ee43306f7ac0392bc7e9f9af861d8ad5
Thanadon99 <thanadon.scp@gmail.com>
2018-05-17 23:03:40 +0700

    3

--- index.php
+++ index.php
@@ -341,6 +341,10 @@ if(!is_null($events)){
 						$strText1 = "";
 						fwrite($myfile, $strText1);
 						fclose($myfile);
+						$myfile = fopen("x.txt", "w") or die("Unable to open file!");
+						$strText1 = "0";
+						fwrite($myfile, strText1);
+						fclose($myfile);
                         // กำหนด action 4 ปุ่ม 4 ประเภท
                         $actionBuilder = array(
                             new DatetimePickerTemplateActionBuilder(
@@ -359,7 +363,7 @@ if(!is_null($events)){
                         $replyData = new TemplateMessageBuilder('Button Template',
                             new ButtonTemplateBuilder(
                                     'เลือกวันที่ปฏิบัติภารกิจ', // กำหนดหัวเรื่อง
-                                    'Please select6', // กำหนดรายละเอียด
+                                    'Please select7', // กำหนดรายละเอียด
                                     $imageUrl, // กำหนด url รุปภาพ
                                     $actionBuilder  // กำหนด action object
                             )
